fa1212dacc15ef67c42eb0c60c6826969fbb4ede
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2025-04-22 05:37:50 -0400

    [ADD] Benchmark randomized Bi-Laplacian (#40)
    * [ADD] Experiment for benchmarking the weighted Laplacian [no ci]
    
    * [FIX] Typo [no ci]
    
    * [ADD] Benchmark results for the weighted Laplacian [no ci]
    
    * [ADD] Add weighted Laplacian results to paper
    
    * [ADD] Support and test randomized weighted Laplacian
    
    * [ADD] Support randomized weighted Laplacian in benchmark
    
    * [ADD] Experiment entry for randomized weighted Laplacian
    
    * [ADD] Implement Monte-Carlo estimated Bi-Laplacian
    
    * [ADD] Test simplification of randomized Bi-Laplacian
    
    * [REF] Rename `...BiLaplacian` -> `...Bilaplacian`
    
    * [ADD] Test Bi-Laplacian implementations of exp01
    
    * [ADD] Support and test randomized Bi-Laplacian in benchmark
    
    * [ADD] Experiment entry for randomized Bi-Laplacian
    
    * [FIX] Change device to CUDA
    
    * [ADD] Benchmark results for randomized Bi-Laplacian
    
    * [TEX] Describe randomized Bi-Laplacian results [no ci]

--- jet/exp/exp01_benchmark_laplacian/run.py
+++ jet/exp/exp01_benchmark_laplacian/run.py
@@ -307,6 +307,27 @@ EXPERIMENTS = [
         # what to plot: x-axis is nums_samples and each strategy is plotted in a curve
         ("num_samples", "strategy"),
     ),
+    # Experiment 6:  Use the largest MLP from dangel2024kroneckerfactored and with
+    #                5 in features, vary the MC samples computing the randomized
+    #                Bi-Laplacian.
+    (  # Experiment name, must be unique
+        "dangel2024kroneckerfactored_bilaplacian_vary_num_samples",
+        # Experiment parameters
+        {
+            "architectures": ["tanh_mlp_768_768_512_512_1"],
+            "dims": [5],
+            "batch_sizes": [256],
+            "strategies": SUPPORTED_STRATEGIES,
+            "devices": ["cuda"],
+            "operator": "bilaplacian",
+            "distributions": ["normal"],
+            # exact takes 6 D**2 - 3D + 6 = 141, randomized takes 2 + 3S, so choosing
+            # S <= 46 because for S=47 we can compute the Bi-Laplacian exactly
+            "nums_samples": linspace(1, 46, 10).int().unique().tolist(),
+        },
+        # what to plot: x-axis is nums_samples and each strategy is plotted in a curve
+        ("num_samples", "strategy"),
+    ),
 ]
 
 if __name__ == "__main__":
